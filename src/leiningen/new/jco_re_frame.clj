9e2eadccf7d36cfbf6b469fb6b3a37a7f1cfe4cc
Jonas Collberg <jonas.collberg@gmail.com>
2018-04-29 11:59:57 +0200

    Add option validation

--- src/leiningen/new/jco_re_frame.clj
+++ src/leiningen/new/jco_re_frame.clj
@@ -16,15 +16,25 @@
 (defn validate-opts [opts]
   (let [invalid-opts (remove (set valid-opts) opts)]
     (cond
-      (seq invalid-opts) (str "Invalid options: " (str/join " " invalid-opts)
+      (seq invalid-opts) (str "Invalid option"
+                              (when (< 1 (count invalid-opts)) "s") ": "
+                              (str/join " " invalid-opts)
                               "\nSupported options: "
                               (str/join " " valid-opts)))))
 
+(defn- template-data
+  [name opts]
+  {:name name
+   :sanitized (name-to-path name)})
+
+(defn- template-files [data]
+  ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])
+
 (defn jco-re-frame
   "Leiningen template to generate re-frame projects."
-  [name]
-  (let [data {:name name
-              :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' re-frame (jco style) project.")
-    (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+  [name & opts]
+  (if-let [error (validate-opts opts)]
+    (println error)
+    (let [data (template-data name opts)]
+      (main/info "Generating fresh 'lein new' re-frame (jco style) project.")
+      (->files data (template-files data)))))
